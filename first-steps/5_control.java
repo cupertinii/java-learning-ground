0e6210980dad49cb0a6d5a198b8747e88e01edfa
aditi <aditya@Adityas-MacBook-Pro.local>
2020-10-16 19:35:34 -0700

    Control Structures

---
+++ first-steps/5_control.java
@@ -0,0 +1,81 @@
+public class App {
+
+  public static void main(String[] args) {
+    if_else_example();
+    if_else_if_example();
+    switch_case_example();
+  }
+
+  public static void if_else_example() {
+
+    // if (condition) { run this code if condition is true }
+    // else { run this code if condition is false }
+    boolean isOn = true;
+    if(isOn) {
+      System.out.println("System is ON");
+    }
+    else {
+      System.out.println("System is OFF");
+    }
+
+    int age = 18;
+    if(age > 18) {
+      System.out.println("Adult");
+    }
+    else {
+      System.out.println("Minor");
+    }
+  }
+
+  public static void if_else_if_example() {
+    int age = 44;
+
+    if(age > 110) {
+      System.out.println("Late Centenary");
+    }
+    else if(age > 100) {
+      System.out.println("Early Centenary");
+    }
+    else if(age > 80) {
+      System.out.println("Late Elder");
+    }
+    else if(age > 65) {
+      System.out.println("Early Elder");
+    }
+    else if(age > 45) {
+      System.out.println("Preelder");
+    }
+    else if(age > 29) {
+      System.out.println("Middle Aged Adult");
+    }
+    else if(age > 25) {
+      System.out.println("Grown Adult");
+    }
+    else if(age > 19) {
+      System.out.println("Young Adult");
+    }
+    else if (age > 12 ) {
+      System.out.println("Teenager");
+    }
+    else if (age > 9 ) {
+      System.out.println("Tweenager");
+    }
+    else if (age > 5 ) {
+      System.out.println("Gradeschooler");
+    }
+    else if (age > 3 ) {
+      System.out.println("Preschooler");
+    }
+    else if (age > 1 ) {
+      System.out.println("Toddler");
+    }
+    else {
+      System.out.println("Baby");
+    }
+  }
+
+  public static void switch_case_example() {
+
+  }
+
+}
