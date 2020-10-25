05e026c02ec9b398f1449f092a3d3b836cb58e83
cupertinii <aditi.cupertino@outlook.com>
2020-10-25 02:21:46 -0700

    Added Text and Label

---
+++ 05_gui/02_textfield_label.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class GUIApp {
+  public static void main(String[] args) {
+
+    JFrame frame = new JFrame("Cupertinii Swing GUI");
+    frame.setSize(1280,800);
+    frame.setLayout(null);
+
+    JLabel label = new JLabel();
+    label.setBounds(200, 200, 100, 50);
+
+    JTextField txt = new JTextField("Your Name");
+    txt.setBounds(200, 100, 100, 50);
+
+    txt.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        String textFieldData = txt.getText();
+
+        label.setText("Hi " + textFieldData + " !");
+      }
+    });
+
+    frame.add(label);
+    frame.add(txt);
+
+    frame.setVisible(true);
+  }
+}
