73e93582a672cd844b641a20dc38ce47171d42f9
cupertinii <aditi.cupertino@outlook.com>
2020-10-28 15:22:09 -0700

    Updated with confirmation dialog

--- 05_gui/05_alert_optionpane.java
+++ 05_gui/05_alert_optionpane.java
@@ -7,13 +7,12 @@ public class GUIApp {
     JFrame frame = new JFrame("Cupertinii Swing GUI");
     frame.setSize(500,500);
     frame.setLayout(null);
-    frame.setVisible(true);
 
-    JButton btn = new JButton("Button - Simple Message");
-    btn.setBounds(10, 50, 180, 50);
+    JButton btn_1 = new JButton("Button - Simple Message");
+    btn_1.setBounds(10, 50, 180, 50);
 
     // Show Message Dialog
-    btn.addActionListener(new ActionListener() {
+    btn_1.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
 
         // This will create a alert dialog box
@@ -25,7 +24,31 @@ public class GUIApp {
       }
     });
 
-    frame.add(btn);
+    JButton btn_2 = new JButton("Button - Yes/No");
+    btn_2.setBounds(10, 100, 180, 50);
+
+    // Show Yes/No Message Box
+    btn_2.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+
+        // This will create a alert dialog box with Yes/No buttons
+        int response = JOptionPane.showConfirmDialog(
+          frame,                                    // Parent
+          "Button Clicked!",                        // Message String
+          "Do you agree?",                          // Dialog Title (Optional)
+          JOptionPane.YES_NO_OPTION);               // Message Type (Optional)
+
+        if(response == JOptionPane.NO_OPTION) {
+          System.out.println("Clicked => No");
+        }
+        else if(response == JOptionPane.YES_OPTION) {
+          System.out.println("Clicked => Yes");
+        }
+      }
+    });
+
+    frame.add(btn_1);
+    frame.add(btn_2);
 
     frame.setVisible(true);
   }
